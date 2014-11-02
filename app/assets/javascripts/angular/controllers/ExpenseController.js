49160e5f9ebbd738607b175b8474c5f17dd31715
Ryan Goh <ryangoh@berkeley.edu>
2014-11-02 09:45:59 -0800

    nicer commit

--- app/assets/javascripts/angular/controllers/ExpenseController.js
+++ app/assets/javascripts/angular/controllers/ExpenseController.js
@@ -1,9 +1,9 @@
 myApp.controller('ExpenseController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-    if (!$routeParams.code) {
-      window.location.replace(
-        "https://api.venmo.com/v1/oauth/authorize?client_id=2071&scope=make_payments&response_type=code&redirect_uri=http://splitwit.me/app"
-      );
-    }
+    // if (!$routeParams.code) {
+    //   window.location.replace(
+    //     "https://api.venmo.com/v1/oauth/authorize?client_id=2071&scope=make_payments&response_type=code&redirect_uri=http://splitwit.me/app"
+    //   );
+    // }
 
     console.log($routeParams.code)
 
@@ -78,6 +78,8 @@ myApp.controller('ExpenseController', ['$scope', '$http', '$routeParams', functi
           console.log(data)
         });
     }
+
+    console.log("done initializing")
 
   }
 ]);
