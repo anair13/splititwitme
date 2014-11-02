ac17435688718d30ad9df23c898b917b9b668e10
Ryan Goh <ryangoh@berkeley.edu>
2014-11-02 02:32:58 -0800

    Adding payers and sharers

--- app/assets/javascripts/angular/controllers/AppCtrl.js
+++ app/assets/javascripts/angular/controllers/AppCtrl.js
@@ -3,30 +3,33 @@ myApp.controller('AppCtrl', ['$scope', function($scope) {
 }]);
 
 myApp.controller('ExpenseController', ['$scope', function($scope) {
-    $scope.expenses = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+    $scope.expenses = [];
+    payers = {};
  
     $scope.addExpense = function() {
-      $scope.expenses.push({text:$scope.expenseText, text:$scope.payerText, text:$scope.costText, done:false});
+      $scope.expenses.push({expense:$scope.expenseText, payer:$scope.payerText, cost:$scope.costText, done:false});
+      if (!(payers[$scope.payerText])) {
+        payers[$scope.payerText] = $scope.costText;
+      } else {
+        payers[$scope.payerText] = payers[$scope.payerText] + $scope.costText;
+      }
       $scope.expenseText = '';
       $scope.payerText  = '';
       $scope.costText = '';
     };
- 
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.expenses, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
+
+    $scope.addSharer = function() {
+      if (!(payers[$scope.nameText])) {
+        payers[$scope.nameText] = 0;
+      $scope.sharerText = '';
     };
- 
-    $scope.archive = function() {
-      var oldTodos = $scope.expenses;
-      $scope.expenses = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.expenses.push(todo);
-      });
+
+    $scope.getKeys = function() {
+      return Object.keys(payers);
     };
+
+    $scope.getTotal = function(name) {
+      return payers[name];
+    };
+
   }]);
