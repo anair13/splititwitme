b8504c6770aba26a7332c0cb678cdc3a646f5c1f
Roy Tu <thatsnail@gmail.com>
2014-11-04 01:47:05 -0500

    Bugfix

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -12,26 +12,20 @@ def make_transactions(access_token, payees, charges)
     # access_token -- oauth token for the user received from get_user_authorization()
     # payees       -- array of user ids to pay / charge
     # charges      -- array of amounts to pay / charge (charges are negative)
-    uri = URI("https://api.venmo.com/v1/oauth/payments")
     for (payee, charge) in payees.zip(charges)
-        res = Net::HTTP.post_form(uri,
-            'access_token' => access_token,
-            'user_id'      => payee,
-            'note'         => "Thanks for the money, suckers!",
-            'amount'       => charge,
-            'audience'     => "private"
-        )
+        email = payee
+        note = "Thanks! Processed successfully through splitwitme."
+        amount = charge
+        audience = "private"
+        resp = `curl -X POST -d "access_token=#{access_token}&email=#{email}&note=#{note}&amount=#{amount}&audience=#{audience}" "https://api.venmo.com/v1/payments"`
+        puts(resp)
     end
 end
 
 def get_access_token_and_user_id(client_id, code, client_secret)
     # Takes a client_id, code (from get_user_authorization) and client_secret
     # and returns an [access_token, user_id] pair that can be used with venmo API calls
-    uri = URI("https://api.venmo.com/v1/oauth/access_token")
-    res = Net::HTTP.post_form(uri,
-        'client_id'     => client_id,
-        'code'          => code,
-        'client_secret' => client_secret
-    )
-    return [res["access_token"]] + [res["username"]]
+    resp = JSON.parse(`curl -X POST -d "client_id=#{client_id}&code=#{code}&client_secret=#{client_secret}" "https://api.venmo.com/v1/oauth/access_token"`)
+    puts([resp["access_token"]] + [resp["username"]])
+    return [resp["access_token"]] + [resp["username"]]
 end
